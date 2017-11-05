313800cd9f7579685ac39b2e23d4255c66b32cab
A-ron <codevigilante@gmail.com>
2017-11-05 11:00:42 -0600

    master: updated to use SW0.4.0;

---
+++ README.md
@@ -0,0 +1,12 @@
+# Hello
+
+This is the repository for the highly popular website, Silly Widgets. Thanks for stopping by and looking with your eyes, or however you "see."
+
+# Stuff
+
+* Uses SillyWidgets version 0.4.0
+* Hot to the touch
+
+# todo
+
+* 
--- lambda/controllers/Root.cs
+++ lambda/controllers/Root.cs
@@ -2,8 +2,6 @@ using System;
 using System.Threading.Tasks;
 using System.IO;
 using SillyWidgets;
-using Amazon.S3;
-using Amazon.S3.Model;
 
 namespace SillyWidgetsLambda
 {
@@ -17,15 +15,23 @@ namespace SillyWidgetsLambda
 
         public ISillyView Index(ISillyContext context)
         {
-            SillyView home = new SillyView();
-            home.Content = "Silly Widgets v0.3 - Hello";
+            Task<ISillyView> result = base.LoadViewAsync("sillywidgets.com", "index.html", Amazon.RegionEndpoint.USWest1);
 
-            /*home.Bind("pagetitle", "Silly Widgets v0.3");
+            result.Wait();
+
+            SillyView home = result.Result as SillyView;
+
+            if (home == null)
+            {
+                return(null);
+            }
+            
+            home.Bind("pagetitle", "Silly Widgets v0.3");
             home.Bind("heading", "Silly Widgets v0.3");
             home.Bind("subheading", "A silly little C# MVC framework for AWS Lambda");
             home.Bind("description", "We don't NEED another Goddamn framework! And this is the part where I'm supposed to say, \"Oh, but this one's different!\" BUT, it really isn't. It's just targeting a different platform. So if you like AWS Lambda, and hate paying for hosting that you never use, and like using C# and .NET, then maybe Silly Widgets will scratch that little spot on your taint that itches.");
             home.Bind("launchdate", "Probably November 2017, but maybe December 2017");
-            home.Bind("contact", "send all inquiries to codevigilante@gmail.com");*/
+            home.Bind("contact", "send all inquiries to codevigilante@gmail.com");
 
             return(home);
         }
